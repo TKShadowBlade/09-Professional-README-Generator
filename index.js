ab46465edb8c43cba9ea7a7fa92273bb5b4c3b25
TKShadowBlade <katanablack85@gmail.com>
2021-09-27 23:36:32 -0400

    updated inquirer options

--- index.js
+++ index.js
@@ -35,6 +35,22 @@ inquirer
         message: 'Instructions for using your app:',
         name: 'usage',
       },
+    {
+        type: 'list',
+        message: 'What license are you using?',
+        name: 'license',
+        choices: ['GNU GPLv3', 'MIT License', 'GNU AGPLv3', 'Apache License 2.0']
+    },
+    {
+        type: 'input',
+        message: 'Include any contributions:',
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: 'Detail any testing instructions:',
+        name: 'test',
+    },
   ])
   
   
