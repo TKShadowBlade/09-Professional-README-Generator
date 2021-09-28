71c6420bf67b49ad7c7024309cb09796232de55b
TKShadowBlade <katanablack85@gmail.com>
2021-09-27 23:13:12 -0400

    inquirer prompts

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
+//importing node modules
 const inquirer = require('inquirer');
+const fs = require('fs');
 
+//inquirer questions
 inquirer
   .prompt([
     {
       type: 'input',
       message: 'What is the name of your application?',
-      name: 'app name',
+      name: 'title',
     },
     {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+      },
+    {
       type: 'input',
-      message: 'What is your Github username?',
-      name: 'username',
-    },
+      message: 'What is your email address?',
+      name: 'email',
+    },  
     {
       type: 'input',
       message: 'Write a description for your app:',
       name: 'description',
     },
+    {
+      type: 'input',
+      message: 'Installation instructions for your app:',
+      name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Instructions for using your app:',
+        name: 'usage',
+      },
   ])
+  
   
